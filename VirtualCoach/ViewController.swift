24de179ff962bc3aff7afbb82382a05c3a95e915
Victor Nazario <victor.nazario@upr.edu>
2018-04-07 15:17:50 -0400

    Initial Commit

---
+++ VirtualCoach/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VirtualCoach
+//
+//  Created by Victor Nazario on 4/7/18.
+//  Copyright © 2018 Victor Nazario. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
